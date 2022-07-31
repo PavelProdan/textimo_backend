a12810acb301a5b92b7985da21d58b675006c316
Pavel Prodan <prodanpavel05@gmail.com>
2022-07-31 15:20:30 +0300

    commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 //this is the main app
 const express = require("express");
-const { port } = require('./config/config.js');
-
 const app = express();
 
-app.get("/test", (req, res) => {
-  res.json({ ok: true });
-});
+const { port } = require('./config/config.js');
+const CheckForConnectionRoute = require("./routes/CheckForConnection.js");
+
+app.use("/CheckForConnection", CheckForConnectionRoute);
 
 app.listen(port, () => console.log("Server is listening on port " + port));
