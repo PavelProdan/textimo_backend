7fb48366e279d2f1c6e139e4ec93169de653b5a6
Pavel Prodan <prodanpavel05@gmail.com>
2022-07-31 14:49:16 +0300

    update to index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //this is the main app
+const express = require("express");
+const PORT = 3000;
+
+const app = express();
+
+app.get("/test", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(PORT, () => console.log("Server is listening on port " + PORT));
